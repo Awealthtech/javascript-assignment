4a70dfd0e25c5178816a464491bb7bf7cd5b5381
Okunade Adeola <adeolafunke7@gmail.com>
2023-03-17 09:24:46 +0100

    otp

---
+++ otp/main.js
@@ -0,0 +1,10 @@
+const generateotp = () =>{
+    let otp_code = [];
+    for(let i=0; i < 5; i++){
+        const element = Math.round(Math.random() * 9);
+        otp_code.push(element);
+    }
+    const splitted = otp_code.join("");
+    return splitted;
+};
+console.log(generateotp());
