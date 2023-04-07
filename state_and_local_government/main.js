9bf97ee43e763c3610c81c2c8127c293f264958c
Okunade Adeola <adeolafunke7@gmail.com>
2023-04-07 01:02:10 +0100

    states and local government by json

---
+++ state_and_local_government/main.js
@@ -0,0 +1,15 @@
+
+const generate = () =>{
+    function generateotp(){
+        let otp_code = [];
+        for(let i=0; i < 5; i++){
+            const element = Math.round(Math.random() * 9);
+            otp_code.push(element);
+        }
+        const splitted = otp_code.join("");
+        return splitted;
+    };
+    
+    document.getElementById("display_otp").innerHTML = generateotp();
+    console.log(generateotp());
+}
